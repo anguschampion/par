ea7d73636060d4a1bcf3e9adfdd4989ddd15146c
Angus Champion <anguschampion@gmail.com>
2025-06-16 15:33:54 +0100

    Updated font size

--- widget-short.js
+++ widget-short.js
@@ -110,7 +110,7 @@
     border-radius: 15px;
     padding: 10px 15px;
     font-family: 'Roboto Mono', monospace;
-    font-size:28px;
+    font-size:24px;
     font-weight: bold;
     text-align: center;
     display: flex;
